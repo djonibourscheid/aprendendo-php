5a7fbe10a43cc06671bf6a3cb6a6e3e942084455
Djoni Bourscheid <djonibourscheid00@gmail.com>
2023-09-12 10:53:35 -0300

    aparando strings

--- 01-Aprenda-a-programar-em-php/04-Tratando-strings/aula3/manipulando-nomes.php
+++ 01-Aprenda-a-programar-em-php/04-Tratando-strings/aula3/manipulando-nomes.php
@@ -6,6 +6,18 @@ $nomeCompleto = "Djoni Bourscheid";
 [$nome, $sobrenome] = explode(' ', $nomeCompleto);
 echo "Nome: " . $nome . PHP_EOL . "Sobrenome: " . $sobrenome . PHP_EOL;
 
+
 // Unindo
 $novoNome = implode(' ', [$nome, 'L.', $sobrenome]);
 echo PHP_EOL . $novoNome . PHP_EOL;
+
+$telefones = ['(51) 9999-9999', '(11) 99999-9999'];
+echo implode(' | ', $telefones) . PHP_EOL;
+
+
+// Aparando stigns, apenas as laterais.
+$csv = ',,.Djoni Bourscheid,  .';
+
+echo trim($csv, ',. ') . PHP_EOL;
+echo rtrim($csv, ',. ') . PHP_EOL;
+echo ltrim($csv, ',. ') . PHP_EOL;
