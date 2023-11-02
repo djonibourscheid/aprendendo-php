0261526db29254c1c88e437631bb6e5e05117ae8
Djoni Bourscheid <djonibourscheid00@gmail.com>
2023-11-02 21:52:49 +0000

    Try catch e Multi-catch

--- 01-Aprenda-a-programar-em-php/08-PHP-Exceptions/pilha.php
+++ 01-Aprenda-a-programar-em-php/08-PHP-Exceptions/pilha.php
@@ -4,20 +4,31 @@ function funcao1()
 {
   echo 'Entrei na função 1' . PHP_EOL;
 
-  // RuntimeException
-  // $arrayFixo = new SplFixedArray(2);
-  // $arrayFixo[4] = 'Valor';
+  try {
+    funcao2();
+  } catch (RuntimeException | DivisionByZeroError $erroOuExcecao) {
+    echo $erroOuExcecao->getMessage() . PHP_EOL;
+    echo 'Linha de erro ' . $erroOuExcecao->getLine() . PHP_EOL;
+    echo $erroOuExcecao->getTraceAsString() . PHP_EOL;
 
-  // DivisionByZeroError
-  // $divisao = intdiv(5, 0);
+    echo '-> Na função 1, eu resolvi um erro na função 2' . PHP_EOL;
+  }
 
-  funcao2();
   echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
   echo 'Entrei na função 2' . PHP_EOL;
+
+  // RuntimeException
+  $arrayFixo = new SplFixedArray(2);
+  $arrayFixo[4] = 'Valor';
+
+  // DivisionByZeroError
+  $divisao = intdiv(5, 0);
+
+
   for ($i = 1; $i <= 5; $i++) {
     echo $i . PHP_EOL;
   }
