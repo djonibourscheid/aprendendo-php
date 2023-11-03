68938b49346e63b74ee07e5392831cc8d54dfef9
Djoni Bourscheid <djonibourscheid00@gmail.com>
2023-11-03 17:03:24 +0000

    Pegando problemas genericos com Throwable

--- 01-Aprenda-a-programar-em-php/08-PHP-Exceptions/criando-excecao.php
+++ 01-Aprenda-a-programar-em-php/08-PHP-Exceptions/criando-excecao.php
@@ -1,16 +1,38 @@
 <?php
 
-function somar($a, $b)
+function funcao1()
 {
-  $soma = $a + $b;
-  echo $soma;
+  echo 'Entrei na função 1' . PHP_EOL;
+
+  try {
+    funcao2();
+  } catch (Throwable $problema) { // pegando qualquer problema sendo error ou exception
+    echo '---------' . PHP_EOL;
+    echo $problema->getMessage() . PHP_EOL;
+    echo 'Linha de erro ' . $problema->getLine() . PHP_EOL;
+    echo $problema->getTraceAsString() . PHP_EOL;
+
+    echo '-> Na função 1, eu resolvi um erro na função 2' . PHP_EOL;
+    echo '---------' . PHP_EOL;
+  }
+
+  echo 'Saindo da função 1' . PHP_EOL;
 }
 
+/**
+ * @throws Exception
+ */
+function funcao2()
+{
+  echo 'Entrei na função 2' . PHP_EOL;
 
+  // lançando excecao
+  throw new RuntimeException("Esta é a mensagem da exceção");
 
-$excecao = new Exception;
-// lançando excecao
-throw ($excecao);
+  // Código nunca executado
+  echo 'Saindo da função 2' . PHP_EOL;
+}
 
-// Código nunca executado
-somar(1, 2);
+echo 'Iniciando o programa principal' . PHP_EOL;
+funcao1();
+echo 'Finalizando o programa principal' . PHP_EOL;
